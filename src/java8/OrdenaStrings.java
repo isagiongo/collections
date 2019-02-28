51377d658a9054bd2c7c79476375d9b7e9cbb936
Isa Giongo <isagiongo@gmail.com>
2019-02-27 21:08:15 -0300

    Inicia implementação com lambdas

--- src/java8/OrdenaStrings.java
+++ src/java8/OrdenaStrings.java
@@ -16,37 +16,46 @@ public class OrdenaStrings {
 		palavras.add("Pupy Giongo");
 		palavras.add("Teddy Torres");
 		Collections.sort(palavras);
-		System.out.println(palavras);
+		System.out.println("Adiciona na lista com add " + palavras);
 
 		List<String> outrasPalavras = Arrays.asList("Isadora Giongo", "Veronica Torres", "Pupy Giongo", "Teddy Torres");
 		Collections.sort(outrasPalavras);
-		System.out.println(outrasPalavras);
+		System.out.println("Adiciona com arrays.asList " + outrasPalavras);
 
 		// Utilizando o comparator é possível comparar de outra forma, que não a
 		// alfabética. Por exemplo, por tamanho da palavra.
 		Comparator<String> comparador = new ComparadorPorTamanho();
 		// Collections.sort(palavras, comparador);
 		palavras.sort(comparador); // java 8
-		System.out.println(palavras);
+		System.out.println("Utiliza comparator e método para comparar palavras por tamanho " + palavras);
+		
+		//substitui o código acima e o método ComparadorPorTamanho
+		palavras.sort((s1, s2)-> Integer.compare(s1.length(), s2.length()));
 
 		System.out.println("Imprimindo através do for: ");
-		for (String string : outrasPalavras) {
+		for (String string : palavras) {
 			System.out.println("-- " + string);
 		}
 
-		Consumer<String> consumidor = new ImprimePorLinhas();
-		System.out.println("Imprimindo pelo consumer");
+		//classe anonima => Para implementar interfaces muito curtas, 
+		//e não ter que criar uma nova classe para fazer pouca coisa e que não será reaproveitada
+		Consumer<String> consumidor = new Consumer<String>() {
+			@Override
+			public void accept(String t) {
+				System.out.println(t);
+			}
+		};
+		System.out.println("----Imprimindo pelo consumer");
 		palavras.forEach(consumidor);
+		
+		//faz mesma coisa que todo o código acima -- forEach => default method de Iterable - interface que fornece comportamento
+		System.out.println("----for each com lambda");
+		palavras.forEach(s -> System.out.println(s));
+		
+		
 	}
 }
-
-class ImprimePorLinhas implements Consumer<String> {
-	@Override
-	public void accept(String s) {
-		System.out.println("-- " + s);
-	}
-}
-
+									//Interface funcional => só tem um método abstrato
 class ComparadorPorTamanho implements Comparator<String> {
 	@Override
 	public int compare(String s1, String s2) {
